4ec68475b82b4154078bebe641e135c9ed2986e2
black <black@example.com>
2022-11-06 10:54:53 +0500

    initial commit

---
+++ helloapp/helloapp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Program
+{
+    public class ABOBA
+    {
+        private static void Main(string[] args)
+        {
+            SayHello();
+        }
+
+        private static void SayHello()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
